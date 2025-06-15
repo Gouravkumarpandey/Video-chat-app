5ff78c8a1a33b01292b341a195c746b800ca73ae
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-06-15 07:35:05 +0530

    add

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { useSocket} from '../providers/Socket'
+
+
+
+const Homepage =() =>{
+    const {socket} = useSocket();
+
+    const [email, setEmail]  = useState();
+    const [roomId, setRoomId] = useState();
+
+
+
+    const handleJoinRoom = () =>{
+        socket.emit("join-room" , {emailId: email, roomId });
+ };
+
+return (
+    <div className="homepage-container"> 
+            <div className="input-container">
+            <input value={email} onChange={ e => setEmail(e.target.value)} type="email" placeholder='Enter your email here '/>
+            <input value={roomId} onChange={ e => setRoomId(e.target.value)} type="text" placeholder='Enter Room Code '  />
+            <button onClick={handleJoinRoom }>Enter Room</button>
+            </div> 
+    </div>
+    );
+} ;
+
+export default Homepage;
