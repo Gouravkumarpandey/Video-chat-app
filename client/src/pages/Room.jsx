12d437790307affcf66333ab37aec6fb29dad064
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-06-16 15:09:02 +0530

    done

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import {} from "../providers/Socket";
+import { useSocket } from "../providers/Socket";
 
 const RoomPage =() =>{
+    const { socket } = useSocket ();
+
+    const handleNewUserJoined =(data) =>{
+        const {emailId} = data 
+        console.log('New user joined the room',emailId );
+
+
+    }
+
+    useEffect (() =>{
+        socket.on('user-joined', handleNewUserJoined);
+    }, [socket]);
+
     return (
         <div className='room-page-container'>
             <h1>Room Page</h1>
