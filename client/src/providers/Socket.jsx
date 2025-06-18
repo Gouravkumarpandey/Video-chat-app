af829779a2cc3a025311fc0b0e5f078728b5f461
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-06-18 20:33:52 +0530

    Socket.jsx fixed

--- client/src/providers/Socket.jsx
+++ client/src/providers/Socket.jsx
@@ -1,18 +1,22 @@
-import React, { useMemo} from "react";
-import {io} from "socket.io-client";
+import React, { useMemo } from "react";
+import { io } from "socket.io-client";
 
 const SocketContext = React.createContext(null);
 
-export const useSocket =() =>{
-    return React.useContext(SocketContext);
+export const useSocket = () => {
+  return React.useContext(SocketContext);
 };
 
-export const SocketProvider = (props) =>{
-    const socket = useMemo( ( )=>  io("http://localhost:8001"),  []);
-    return(
-        <SocketContext.Provider value={{socket}}  >
-            {props.children}
-        </SocketContext.Provider>
+export const SocketProvider = (props) => {
+  const socket = useMemo(() => io("https://video-chat-app-bcyv.onrender.com", {
+    transports: ["websocket"],
+    reconnection: true,
+    autoConnect: true,
+  }), []);
 
-    )
-}
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {props.children}
+    </SocketContext.Provider>
+  );
+};
